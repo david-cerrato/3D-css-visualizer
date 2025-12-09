488aa114637e858422a9a08dbb89985a43aa0d46
dcerrato001 <david.cerratofer@gmail.com>
2025-12-09 22:14:15 +0100

    Cube and controls

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,54 @@ import { Cube } from './components/cube/Cube'
 
 function App() {
   const [isIsometric, setIsIsometric] = useState<boolean>(false)
+  const [translateX, setTranslateX] = useState<number>(0)
+  const [translateY, setTranslateY] = useState<number>(0)
+  const [translateZ, setTranslateZ] = useState<number>(0)
+  const [rotateX, setRotateX] = useState<number>(0)
+  const [rotateY, setRotateY] = useState<number>(0)
+  const [rotateZ, setRotateZ] = useState<number>(0)
 
   return (
     <>
       <div className="overflow-hidden">
         <div className="viewport" data-isometric={isIsometric}>
-          <Cube></Cube>
+          <Cube 
+            translateX={translateX} 
+            translateY={translateY} 
+            translateZ={translateZ} 
+            rotateX={rotateX} 
+            rotateY={rotateY} 
+            rotateZ={rotateZ}
+          ></Cube>
           <div className="axis y-axis"></div>
           <div className="axis x-axis"></div>
           <div className="axis z-axis"></div>
+        </div>
+      </div>
+      <div className="controls">
+        <div className="range-input">
+          <label htmlFor="translateX">Translate X</label>
+          <input type="number" name="" id="translateX" value={translateX} onChange={(e: any) => setTranslateX(e.target.value)}/>
+        </div>
+        <div className="range-input">
+          <label htmlFor="translateY">Translate Y</label>
+          <input type="number" name="" id="translateY" value={translateY} onChange={(e: any) => setTranslateY(e.target.value)}/>
+        </div>
+        <div className="range-input">
+          <label htmlFor="translateZ">Translate Z</label>
+          <input type="number" name="" id="translateZ" value={translateZ} onChange={(e: any) => setTranslateZ(e.target.value)}/>
+        </div>
+        <div className="range-input">
+          <label htmlFor="rotateX">Rotate X</label>
+          <input type="range" name="" id="rotateX" min="0" max="360" value={rotateX} onChange={(e: any) => setRotateX(e.target.value)}/>
+        </div>
+        <div className="range-input">
+          <label htmlFor="rotateY">Rotate Y</label>
+          <input type="range" name="" id="rotateY" min="0" max="360" value={rotateY} onChange={(e: any) => setRotateY(e.target.value)}/>
+        </div>
+        <div className="range-input">
+          <label htmlFor="rotateZ">Rotate Z</label>
+          <input type="range" name="" id="rotateZ" min="0" max="360" value={rotateZ} onChange={(e: any) => setRotateZ(e.target.value)}/>
         </div>
       </div>
 
--- src/components/cube/Cube.tsx
+++ src/components/cube/Cube.tsx
@@ -1,9 +1,26 @@
+interface CubeProps {
+    translateX: number
+    translateY: number
+    translateZ: number
+    rotateX: number
+    rotateY: number
+    rotateZ: number
+}
 
-
-export function Cube() {
+export function Cube({translateX, translateY, translateZ, rotateX, rotateY, rotateZ}: CubeProps) {
     return (
         <>
-            <div className="cube">
+            <div 
+                className="cube" 
+                style={{
+                    "--translateX": `${translateX}px`,
+                    "--translateY": `${translateY}px`,
+                    "--translateZ": `${translateZ}px`,
+                    "--rotateX": `${rotateX}deg`,
+                    "--rotateY": `${rotateY}deg`,
+                    "--rotateZ": `${rotateZ}deg`
+                    } as React.CSSProperties}
+                >
                 <div className="side side-A"></div>
                 <div className="side side-B"></div>
                 <div className="side side-C"></div>
